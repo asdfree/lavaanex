efa2caf8f5b6b685df46b77d6db201750438de3a
Anthony Damico <ajdamico@gmail.com>
2018-01-13 07:12:51 -0500

    savecache=FALSE inside syntaxtractor

--- tests/setup.R
+++ tests/setup.R
@@ -1,5 +1,6 @@
-options("lodown.cachaca.savecache"=FALSE)
+if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+options("lodown.cachaca.savecache"=FALSE)
 library(lodown)
 library(lavaan.survey)	
 options( survey.lonely.psu = "adjust" )
